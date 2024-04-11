ac1338c2f7a168dd07900a3b0c661f2d2c8f5897
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-04-11 15:28:27 +0500

    Add Question 01.

---
+++ app.js
@@ -0,0 +1,67 @@
+Question # 01
+var firstName= prompt("Enter Your First Name")
+var lastName= prompt("Enter Last Name")
+var fullName= firstName + " " + lastName;
+
+// var result= alert(`Hello ${fullName}`)
+
+// Question # 02
+// var favouritePhone= prompt("Enter Your Favourite Phone")
+
+// document.write(`My Favourite Phone Is: ${favouritePhone} <br> Length of string: ${favouritePhone.length}`)
+
+// // Question # 03
+// var country= prompt("Enter Your Country")
+// var find= prompt("Enter a Alphabet")
+
+// document.write(`String: ${country} <br> Index no of ${find}: ${country.indexOf(find)}`)
+
+// Question # 04
+// var letter= prompt("Enter a Word")
+// var finding= prompt("Enter a Alphabet")
+
+// document.write(`String: ${letter} <br> Last Index of ${finding}: ${letter.lastIndexOf(finding)}`)
+
+// Question # 05
+// var third= prompt("Enter a Word")
+// var index= third.charAt(3)
+
+// document.write(`String: ${third} <br> Character at index 3: ${index}`)
+
+// Question # 06
+// var fName= prompt("Enter First Name")
+// var lName= prompt("Enter Last Name")
+
+// alert (`Welcome ${fName.concat(" " + lName)}`)
+
+// Question # 07
+// var enterWord= ("Hyderabad")
+// var result= enterWord.replace ("Hyderabad", "Islamabad")
+
+// document.write(`City Entered:  ${enterWord} <br> After Replacement: ${result}`)
+
+// Question # 08
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// var result= message.replace (/and/g , "&");
+
+// document.write(message + "<br>" + result)
+
+// Question # 09
+// document.write(`Value: 475 <br> Type: ${typeof "475"} <br> Value: 475 <br> Type: ${typeof 475}`)
+
+// Question # 10
+// var userInput= prompt("Enter Any Word")
+// var upperCase= userInput.toUpperCase();
+
+// document.write(`User Input: ${userInput} <br> Upper Case: ${upperCase}`)
+
+// Question # 11
+
+
+// Question # 12
+// var num= prompt("Enter Any Decimal Number")
+// var without = num.toString().replace(".", ""); 
+
+// document.write(without)
+
+// Question # 13
